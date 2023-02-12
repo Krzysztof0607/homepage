7d3792f2ed33e4cccb5bd07fbf46ea12b926b57a
Krzysztof Bromirski <bromirski0607@gmail.com>
2023-02-12 21:42:46 +0100

    readme and functions added correct

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,12 +1,21 @@
-console.log("Mam nadzieję, że udało mi się przebrnąć przez moduł 3")
+{
+    const welcome = () => {
+        console.log("Mam nadzieję, że udało mi się przebrnąć przez moduł 3")
+    }
 
-let button = document.querySelector(".button")
-let body = document.querySelector(".body")
+    welcome();
 
-button.addEventListener("click", () => {
-    body.classList.toggle("body--dark");
-    button.innerText = body.classList.contains("body--dark") ? "Włącz jaśniejsze tło" : "Włącz ciemniejsze tło";
-});
+    const toggleBackground = () => {
 
+        const body = document.querySelector(".body")
+        body.classList.toggle("body--dark");
+        button.innerText = body.classList.contains("body--dark") ? "Włącz jaśniejsze tło" : "Włącz ciemniejsze tło";
+    };
+    const init = () => {
+        const button = document.querySelector(".button")
+        button.addEventListener("click",toggleBackground);
 
-
+    };
+    
+    init();
+};
---
+++ readme.md
@@ -0,0 +1,16 @@
+
+![site](images/demo.gif)
+
+## Demo
+https://krzysztof0607.github.io/homepage/
+## Opis
+The website is small blog about my favorite and so famous artist Eminem. Take a look if You like him too. :) 
+## The page was created with below features, technologies and conventions:
+- ES6+ features
+- BEM convention
+- arrow functions
+- links
+- deviding the page for sections
+- semantic elements of the page: header,main, section, table, forms, nav, ul, li.
+- classes and ids
+- modification of HTML elements with addEventListener
